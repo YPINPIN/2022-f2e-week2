5079fd22e14be87dc5a3639904a6ad7df4115258
YPINPIN <yping611@gmail.com>
2022-11-21 04:31:17 +0800

    [FIX] router

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import store from '../store'
 
 const resetStep = (to, from, next) => {
@@ -113,7 +113,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,12 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/2022-f2e-week2/' : './',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
-      'vue': 'vue/dist/vue.esm-bundler.js'
+      // 'vue': 'vue/dist/vue.esm-bundler.js'
     }
   }
 })
